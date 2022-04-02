257e0f070f83db5766c24206392604d07cc7ef4a
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-04-02 12:52:39 -0600

    linked index.html on load

---
+++ server.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static('public'));
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
