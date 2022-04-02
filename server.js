1842de3033c70c9ea5b04fbfe5ddb2e74e9790da
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-04-02 13:31:51 -0600

    connects to notes.html when get started button is clicked

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(express.static('public'));
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
 );
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
